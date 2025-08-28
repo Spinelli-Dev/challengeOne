96eeff0bf8357767e3d6ffb5b7810d09618433e1
Spinelli-Dev <legitimo.spinelli@gmail.com>
2025-08-28 15:07:14 -0300

    atualização

--- challengeTwo/challengeTwo.js
+++ challengeTwo/challengeTwo.js
@@ -11,3 +11,21 @@ function calculoRank(win, loss){
     return win - loss
     console.log("Você tem: ")
 }
+
+if (resultado <= 10){
+    rankVitorias = "ferro"
+} else if(resultado > 10 && resultado < 21){
+    rankVitorias = "Bronze"
+} else if(resultado > 20 && resultado < 51){
+    rankVitorias = "Prata"
+} else if(resultado > 50 && resultado < 81){
+    rankVitorias = "Ouro"
+} else if(resultado > 80 && resultado < 91){
+    rankVitorias = "Diamante"
+} else if(resultado > 90 && resultado < 101){
+    rankVitorias = "Lendário"
+} else{
+    rankVitorias = "Imortal"
+}
+
+console.log(rankVitorias)
